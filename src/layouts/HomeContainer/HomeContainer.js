aee75eff6c1e1ef39c5ac26fd2c5d8974daefa76
lewisf20 <lewis_frater@yahoo.com>
2020-09-06 22:59:46 +0100

    Added react-typed

--- src/layouts/HomeContainer/HomeContainer.js
+++ src/layouts/HomeContainer/HomeContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import classes from './HomeContainer.module.css';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
+import Typed from 'react-typed';
 
 const HomeContainer = (props) => {
 	const { title } = props;
@@ -13,13 +14,34 @@ const HomeContainer = (props) => {
 
 	return (
 		<div className={classes.HomeContainer}>
-			<h1>Made By Lewis Frater</h1>
-			<p className={classes.projectDetail}>
-				This ReactJS web application has been created to test different APIs on
-				the internet.
-			</p>
+			<div
+				style={{
+					display: 'flex',
+					height: '70px',
+					flexDirection: 'column',
+					justifyContent: 'space-between',
+				}}
+			>
+				<Typed
+					strings={['<h1>Made By Lewis Frater</h1>']}
+					typeSpeed={40}
+					showCursor={false}
+				/>
+				<Typed
+					strings={[
+						'This ReactJS web application has been created to test different APIs on the internet.',
+					]}
+					typeSpeed={20}
+					showCursor={false}
+					startDelay={1500}
+				/>
+			</div>
 
-			<div>
+			<div
+				style={{
+					marginTop: '2rem',
+				}}
+			>
 				<h2>List of Current APIs</h2>
 				<ul>
 					<Button color="primary" className={classes.btn}>

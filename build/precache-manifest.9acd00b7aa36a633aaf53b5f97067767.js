885a3ee68c2a2eceac3b2a63138d859cf238406f
lewisf20 <lewis_frater@yahoo.com>
2020-09-14 18:30:38 +0100

    removed build folder from gitignore

---
+++ build/precache-manifest.9acd00b7aa36a633aaf53b5f97067767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b39f9d1945def15e41be0b2f39abcc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db24c6a3566fefa6d473",
+    "url": "/static/css/main.64396898.chunk.css"
+  },
+  {
+    "revision": "1badd1e6ae6ad5371f6d",
+    "url": "/static/js/2.fa3d8b11.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.fa3d8b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db24c6a3566fefa6d473",
+    "url": "/static/js/main.adf190e0.chunk.js"
+  },
+  {
+    "revision": "6bf47cc91cf99da16610",
+    "url": "/static/js/runtime-main.e9b2f5a7.js"
+  }
+]);
